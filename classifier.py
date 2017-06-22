deee1d4ce03d7aa0f8d7365f5e52eccb88907267
Gilad Lumbroso <gullumbroso@gmail.com>
2017-06-22 19:20:42 +0300

    With bag of words ready repetative_tests function

--- classifier.py
+++ classifier.py
@@ -35,38 +35,66 @@ class Classifier(object):
         pass
 
 
-def load_shuffled_data():
-    x, y = load_dataset()
+def shuffle(x, y):
     pairs_list = list(zip(x, y))
     random.shuffle(pairs_list)
     new_x, new_y = zip(*pairs_list)
     return new_x, new_y
 
 
+def load_shuffled_data():
+    x, y = load_dataset()
+    return shuffle(x, y)
+
+
+def repetitive_test(clf, x, y, num_of_tests):
+
+    # Divide into train set and test set
+    train_size = int(math.floor(TRAIN_SET_SIZE * len(y)))
+    x_train = x[train_size:]
+    y_train = y[train_size:]
+    x_test = x[:train_size]
+    y_test = y[:train_size]
+
+    cul_score = 0
+    for i in range(num_of_tests):
+        shuffle(x_train, y_train)
+        cul_score += clf.fit(x_train, y_train).score(x_test, y_test)
+
+    return cul_score / num_of_tests
+
+
 def train_bag_of_words():
+
     # Get the data
     samples, labels = load_shuffled_data()
-    train_size = int(math.floor(TRAIN_SET_SIZE * len(samples)))
-    x_train = samples[train_size:]
-    y_train = labels[train_size:]
-    x_test = samples[:train_size]
-    y_test = labels[:train_size]
 
     # Transform the titles into vectors
     count_vect = CountVectorizer(ngram_range=(1, 3))
-    X_train_counts = count_vect.fit_transform(x_train)
+    X_train_counts = count_vect.fit_transform(samples)
     tfidf_transformer = TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-    # Train the classifier
-    # clf = MultinomialNB().fit(X_train_tfidf, articles.target)
-    # clf = SGDClassifier().fit(X_train_tfidf, articles.target)
-    # clf = LogisticRegression().fit(X_train_tfidf, articles.target)
-    clf = svm.SVC().fit(X_train_tfidf, y_train)
-    # clf = svm.LinearSVC().fit(X_train_tfidf, y_train)
-    score = clf.score(x_test, y_test)
+    # Divide into train set and test set
+    train_size = int(math.floor(TRAIN_SET_SIZE * len(samples)))
+    x_train = X_train_tfidf[train_size:]
+    y_train = labels[train_size:]
+    x_test = X_train_tfidf[:train_size]
+    y_test = labels[:train_size]
 
-    print(score)
+    # Train the classifier
+    # clf = MultinomialNB().fit(x_train, y_train) # Score:  68
+    # clf = LogisticRegression().fit(x_train, y_train) # Score: 64
+    # clf = svm.SVC().fit(x_train, y_train)  # Score: 58
+
+    sgd = SGDClassifier()
+    linear_svc = svm.LinearSVC()
+
+    sgd_score = repetitive_test(sgd, x_test, y_test, 1000)
+    svc_score = repetitive_test(linear_svc, x_test, y_test, 1000)
+
+    print("SGD: " + str(sgd_score))
+    print("Linear SVC: " + str(svc_score))
 
 
 train_bag_of_words()
