c96fa6cf15a6617462003cdc00242d0e50f02489
Gilad Lumbroso <gullumbroso@gmail.com>
2017-06-22 19:42:19 +0300

    Separate file

--- classifier.py
+++ classifier.py
@@ -22,7 +22,6 @@ from load_headlines import load_dataset
 import load_headlines
 from sklearn import svm, neighbors
 from sklearn.ensemble import RandomForestClassifier
-from google.cloud import language
 
 
 TRAIN_SET_SIZE = 0.8
@@ -87,39 +86,6 @@ def length_array():
     print("NBR:", score_NBR)
     print("RFC:", score_RFC)
 
-length_array()
-
-
-def train_bag_of_words():
-
-    # Get the data
-    samples, labels = load_shuffled_data()
-
-    # Transform the titles into vectors
-    count_vect = CountVectorizer(ngram_range=(1, 3))
-    X_train_counts = count_vect.fit_transform(samples)
-    tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-
-    # Divide into train set and test set
-    train_size = int(math.floor(TRAIN_SET_SIZE * len(samples)))
-    x_train = X_train_tfidf[train_size:]
-    y_train = labels[train_size:]
-    x_test = X_train_tfidf[:train_size]
-    y_test = labels[:train_size]
-
-    # Train the classifier
-    # clf = MultinomialNB().fit(x_train, y_train) # Score:  68
-    # clf = LogisticRegression().fit(x_train, y_train) # Score: 64
-    # clf = svm.SVC().fit(x_train, y_train)  # Score: 58
-
-    sgd = SGDClassifier()
-    linear_svc = svm.LinearSVC()
-
-    sgd_score = repetitive_test(sgd, x_test, y_test, 1000)
-    svc_score = repetitive_test(linear_svc, x_test, y_test, 1000)
-
-
 
 def train_google(language_client):
     x, y = load_shuffled_data()
@@ -148,15 +114,5 @@ def train_google(language_client):
     print("RFC:", score_RFC)
 
 
-language_client = language.Client()
-train_google(language_client)
-
-
-
-# train_bag_of_words()
-    print("SGD: " + str(sgd_score))
-    print("Linear SVC: " + str(svc_score))
-
-
-train_bag_of_words()
-
+# language_client = language.Client()
+# train_google(language_client)
